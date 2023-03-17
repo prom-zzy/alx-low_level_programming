f15497ca0833ec12bb011994f844216f5e612a73
Promise Imafidon <promiseimafidon1@gmail.com>
2023-03-16 17:24:36 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that uses the puts function to print a line
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+};
+
