2e77a11f278f6f3e3b25ab968ac6c8eabb8eb5d3
Promise Imafidon <promiseimafidon1@gmail.com>
2023-03-21 12:30:27 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/* Header files */
+#include "main.h"
+
+/**
+ * _putchar - Prints the alphabets putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
