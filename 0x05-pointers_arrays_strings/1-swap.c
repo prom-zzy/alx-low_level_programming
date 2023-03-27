65a6575494bddad00333a4eecae8403a217f880f
Promise Imafidon <promiseimafidon1@gmail.com>
2023-03-27 12:33:09 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values ot two integers
+ * @a: integar to swap
+ * @b: integar tp swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}	
