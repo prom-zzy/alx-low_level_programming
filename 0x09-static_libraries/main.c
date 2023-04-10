98fb55c0aed8dcb119526abfefc38f30a85d7557
Promise Imafidon <promiseimafidon1@gmail.com>
2023-04-10 10:13:52 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * main - A function that print string using puts protype
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
