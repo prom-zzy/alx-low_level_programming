c66eb8207bed69b2a704d10dd297af898454f3cf
Promise Imafidon <promiseimafidon1@gmail.com>
2023-04-10 11:47:39 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - prints a string to stdout
@@ -9,13 +8,9 @@
  */
 void _puts(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		putchar(s[i]);
-		i++;
+		_putchar(*s++);
 	}
-
-	putchar('\n');
+		_putchar('\n');
 }
